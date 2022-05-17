feace99ae714789033ab1b7d5926b07d240837d1
Codrin Iftimie <iceboxic@gmail.com>
2022-05-17 21:38:31 +0300

    Add Cambridge as an option

--- pages/index.js
+++ pages/index.js
@@ -93,6 +93,7 @@ export default function Form({applianceId}) {
           <option value="edge-sanjose;627a4962f245b23834eb4766,61a904fa66f466f67dd86cf3">San Jose, Ca - Hopper St</option>
           <option value="edge-lasvegas;627a4962f245b23834eb4766,61a904fa66f466f67dd86cf3">Las Vegas, NV - Party Rd</option>
           <option value="edge-whitefish;627e94767c90aaab2cfd97a3,627e94b17c90aaab5a71a8b5">Whitefish, MT - Parkway Dr</option>
+          <option value="edge-cambridge;627a4962f245b23834eb4766,61a904fa66f466f67dd86cf3">Cambridge, MA  Main St</option>
         </select>
 
         <button type="submit" disabled={isSubmitting}>
